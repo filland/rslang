c9c30e814bf9e60511c7f22bec4f89ae8fa5b622
Student <borodichalexander97@gmail.com>
2020-06-22 21:23:04 +0300

    feat: add basic html template Game page and component AudioGroup

---
+++ src/component/games/audioChallenge/components/AudioGroup/AudioGroup.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Button, Image } from "react-bootstrap";
+import "../../../styles.css";
+
+export const AudioGroup = (props) => {
+  const { word, imgURL } = props;
+
+  return (
+    <div className="audioGroup">
+      <Image src={imgURL} height="130" roundedCircle />
+      <Button className="btn-playAudio" variant="outline-primary">
+        <Image src="https://image.flaticon.com/icons/svg/727/727269.svg" height="70" roundedCircle />
+      </Button>
+      <span>{word}</span>
+    </div>
+  );
+};
+
+AudioGroup.propTypes = {
+  imgURL: PropTypes.string.isRequired,
+  word: PropTypes.string.isRequired,
+};
---
+++ src/component/games/audioChallenge/pages/Game.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { ButtonGroup, Button, ProgressBar } from "react-bootstrap";
+import { AudioGroup } from "../components/AudioGroup/AudioGroup/AudioGroup";
+import "../styles.css";
+
+const nowProgress = 20;
+const words = [
+  { word: "word1", id: 1 },
+  { word: "word2", id: 2 },
+  { word: "word3", id: 3 },
+  { word: "word4", id: 4 },
+  { word: "word5", id: 5 },
+];
+const rightWord = { word: "word1", image: "https://sinkvein.ru/wp-content/uploads/2018/10/%D1%81%D0%BB%D0%BE%D0%B2%D0%BE-300x170.png" };
+
+export const Game = () => (
+  <div className="audioChallenge">
+    <ProgressBar className="progressBar" variant="info" now={nowProgress} label={`${nowProgress}%`} srOnly />
+    <AudioGroup word={rightWord.word} imgURL={rightWord.image} />
+    <Button className="btn-close" variant="outline-danger">Close</Button>
+    <ButtonGroup aria-label="List words">
+      {words.map(({ word, id }) => <Button key={id}>{word}</Button>)}
+    </ButtonGroup>
+    <Button variant="outline-primary">Не знаю</Button>
+  </div>
+);
