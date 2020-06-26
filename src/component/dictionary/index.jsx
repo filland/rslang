1ba8c7c59083578fdb74a57c5a4b24f4ca2a9796
Agnia <agnia2000@inbox.ru>
2020-06-27 00:47:00 +0300

    feat: add propTypes

--- src/component/dictionary/index.jsx
+++ src/component/dictionary/index.jsx
@@ -19,8 +19,16 @@ import {
 const propTypes = {
   fetchWord: PropTypes.func.isRequired,
   words: PropTypes.arrayOf(PropTypes.object).isRequired,
+  wordsDifficult: PropTypes.arrayOf(PropTypes.object).isRequired,
+  wordsDeleted: PropTypes.arrayOf(PropTypes.object).isRequired,
+
   wordCount: PropTypes.number.isRequired,
   wordCountToday: PropTypes.number.isRequired,
+  wordCountDifficult: PropTypes.number.isRequired,
+  wordCountTodayDifficult: PropTypes.number.isRequired,
+  wordCountDeleted: PropTypes.number.isRequired,
+  wordCountTodayDeleted: PropTypes.number.isRequired,
+
   isLoading: PropTypes.bool.isRequired,
 };
 
@@ -48,11 +56,21 @@ class Dictionary extends Component {
             {words.map((item, i) => <CardWord key={i} word={item} />)}
           </CardDeck>
         </Tab>
-        <Tab eventKey="difficult" title="Сложные слова" disabled>
-          tab-2
+        <Tab eventKey="difficult" title="Сложные слова">
+          <div className="my-4">
+            {`Число слов: ${wordCountDifficult} (${wordCountToday} сегодня)`}
+          </div>
+          <CardDeck className="my-4 justify-content-between">
+            {words.map((item, i) => <CardWord key={i} word={item} />)}
+          </CardDeck>
         </Tab>
-        <Tab eventKey="deleted" title="Удалённые слова" disabled>
-          tab-3
+        <Tab eventKey="deleted" title="Удалённые слова">
+          <div className="my-4">
+            {`Число слов: ${wordCount} (${wordCountToday} сегодня)`}
+          </div>
+          <CardDeck className="my-4 justify-content-between">
+            {words.map((item, i) => <CardWord key={i} word={item} />)}
+          </CardDeck>
         </Tab>
       </Tabs>
     );
