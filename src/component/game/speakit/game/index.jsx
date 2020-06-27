490cedbc4376b0b4cff8841b1e1d0266fb70309a
filland <kubalexei@gmail.com>
2020-06-27 03:45:23 +0300

    feat: example of subscribing to user and dict words in SpeakIt game

--- src/component/game/speakit/game/index.jsx
+++ src/component/game/speakit/game/index.jsx
@@ -1,40 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Word from '../word';
 import VoiceInput from '../voice-input';
 import Stepper from '../stepper';
 import blankPicture from '../assets/blank.jpg';
 import './styles.scss';
+import { getDictionaryWords, getUserWords } from '../../../common/word/selectors';
 
 class Game extends Component {
-    render() {
-        return (<div className="speakit-container">
-            <div className="speakit">
-                <Stepper></Stepper>
-                <div className="picture-container">
-                    <img className="picture" src={blankPicture} alt="test" />
-                </div>
-                <VoiceInput></VoiceInput>
-                <div className="words">
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                    <Word></Word>
-                </div>
-                <div className="controls">
-                    <Button>Restart</Button>
-                    <Button className="big-button">Speak please</Button>
-                    <Button>Finish</Button>
-                </div>
-            </div>
-        </div >);
-    }
+  render() {
+    const { dictionaryWords, userWords } = this.props;
+
+    const wordsTemplate = dictionaryWords
+      .filter((val, index) => index <= 9)
+      .map((word) => (<Word key={word.id} word={word}></Word>));
+
+    return (<div className="speakit-container">
+      <div className="speakit">
+        <Stepper></Stepper>
+        <div className="picture-container">
+          <img className="picture" src={blankPicture} alt="test" />
+        </div>
+        <VoiceInput></VoiceInput>
+        <div className="words">
+          {wordsTemplate}
+        </div>
+        <div className="controls">
+          <Button>Restart</Button>
+          <Button className="big-button">Speak please</Button>
+          <Button>Finish</Button>
+        </div>
+      </div>
+    </div>);
+  }
 }
 
-export default Game;
+const mapStateToProps = (store) => ({
+  dictionaryWords: getDictionaryWords(store),
+  userWords: getUserWords(store),
+});
+
+const mapDispatchToProps = {
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Game);
