f093c7737c5fd2fe60b73966fa8e4e653c4eea47
Marina Poprotskaya <mitsevich.marina@gmail.com>
2020-06-27 20:58:21 +0300

    feat: add statictics with chart.js graph

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './component/navbar';
 import Login from './component/login';
 import Registration from './component/registration';
 import Settings from './component/settings';
-import Dictionary from "./component/dictionary";
+import Dictionary from './component/dictionary';
 import Weather from './component/weather';
 import EnglishPuzzle from './component/english-puzzle/GamePage/GamePage';
 import SpeakIt from './component/game/speakit';
-import AuthProvider from "./component/common/auth-provider";
+import AuthProvider from './component/common/auth-provider';
+import LineStatistics from './component/long-term-statistics';
 
 import './App.scss';
 
@@ -25,6 +26,7 @@ function App() {
           <Route path="/dictionary" component={Dictionary} />
           <Route path="/english-puzzle" component={EnglishPuzzle} />
           <Route path="/game/speakit" component={SpeakIt} />
+          <Route path="/long-term-statistics" component={LineStatistics} />
         </AuthProvider>
       </div>
     </Router>
