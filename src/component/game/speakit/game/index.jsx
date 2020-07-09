1920aaf0a43f5687b86733c4833106189a5558f9
filland <kubalexei@gmail.com>
2020-07-09 03:20:41 +0300

    feat: add mvp of speakit game

--- src/component/game/speakit/game/index.jsx
+++ src/component/game/speakit/game/index.jsx
@@ -3,31 +3,148 @@ import { connect } from 'react-redux';
 import Button from 'react-bootstrap/Button';
 import Word from '../word';
 import VoiceInput from '../voice-input';
-import Stepper from '../stepper';
+import Progress from '../progress';
 import blankPicture from '../assets/blank.jpg';
 import './styles.scss';
 import { prepareWords } from '../../../../common/helper/WordsHelper';
 
 class Game extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      preparedWords: [],
+      picUrl: blankPicture,
+      voiceRecognitionEnabled: false,
+      recognizedWord: '',
+      stars: 0,
+    };
+  }
+
+  startGame = () => {
     const { prepareWords } = this.props;
-    const { preparedWords } = prepareWords(10);
-    const wordsTemplate = preparedWords.map((word) => (<Word key={word.id} word={word}></Word>));
+    const { preparedWords, newWordsNumber } = prepareWords(10);
+    this.setState({
+      preparedWords,
+      picUrl: blankPicture,
+      voiceRecognitionEnabled: false,
+      recognizedWord: '',
+      stars: 0,
+    });
+  }
+
+  componentWillUnmount() {
+    this.recognition.stop();
+  }
+
+  componentDidMount() {
+    this.startGame();
+    // if voice recognition is on then try to check the word with the responce of voice recongnition
+    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    this.recognition = new SpeechRecognition();
+    this.recognition.maxAlternatives = 1;
+    this.recognition.lang = 'en-US';
+    this.recognition.addEventListener('result', (event) => {
+      const recognizedWord = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+      const { voiceRecognitionEnabled } = this.state;
+      if (voiceRecognitionEnabled) {
+        this.setState({ recognizedWord });
+        this.checkRecognizedWord(recognizedWord);
+      }
+    });
+    this.recognition.addEventListener('end', () => {
+      this.recognition.start();
+    });
+    this.recognition.start();
+  }
+
+  checkRecognizedWord = (recognizedWord) => {
+    const { preparedWords, stars } = this.state;
+
+    for (let i = 0; i < preparedWords.length; i += 1) {
+      const word = preparedWords[i];
+
+      if (word.word.toLowerCase() === recognizedWord.toLowerCase()) {
+        word.answered = true;
+        this.setState({ stars: stars + 1 });
+
+        // check if the game finished
+        this.checkGameFinished();
+      }
+    }
+  }
+
+  checkGameFinished = () => {
+    const { preparedWords } = this.state;
+
+    let gameFinished = true;
+    preparedWords.forEach((word) => {
+      if (!word.answered) {
+        gameFinished = false;
+      }
+    });
+
+    if (gameFinished) {
+      // navigate to Statistics page
+      console.log("SpeakIt game is finished");
+    }
+  }
+
+  speakPleaseClick = () => {
+    const newVal = !this.state.voiceRecognitionEnabled;
+    this.setState({ voiceRecognitionEnabled: newVal });
+  }
+
+  handleWordClick = (wordTranslate, picUrl, callback) => {
+    const { voiceRecognitionEnabled } = this.state;
+
+    if (!voiceRecognitionEnabled) {
+      this.setState({ picUrl, wordTranslate });
+      callback();
+    }
+  }
+
+  // do I really need this method and the button Finish ?
+  finishGame = () => {
+    // prepare statistics
+
+    // count failed attempts for each word
+
+    // navigate to statistics page
+
+    // add statistics to the app's statistics
+  }
+
+  render() {
+    const {
+      preparedWords, picUrl, wordTranslate, voiceRecognitionEnabled, recognizedWord, stars,
+    } = this.state;
+
+    const wordsTemplate = preparedWords.map((word) => (
+      <Word
+        key={word.id}
+        word={word}
+        handleClick={this.handleWordClick}></Word>
+    ));
+
+    const speakPleaseClickedStyle = voiceRecognitionEnabled ? { backgroundColor: 'red' } : null;
 
     return (<div className="speakit-container">
       <div className="speakit">
-        <Stepper></Stepper>
+        <Progress stars={stars}></Progress>
         <div className="picture-container">
-          <img className="picture" src={blankPicture} alt="test" />
+          <img className="picture" src={picUrl} alt="SpeakIt game" />
         </div>
-        <VoiceInput></VoiceInput>
+        {voiceRecognitionEnabled ? <VoiceInput recognizedWord={recognizedWord}></VoiceInput> : <div className="word-translation">{wordTranslate}</div>}
         <div className="words">
           {wordsTemplate}
         </div>
         <div className="controls">
-          <Button>Restart</Button>
-          <Button className="big-button">Speak please</Button>
-          <Button>Finish</Button>
+          <Button onClick={this.startGame}>Restart</Button>
+          <Button className="big-button" style={speakPleaseClickedStyle} onClick={this.speakPleaseClick}>Speak please</Button>
+          <Button onClick={this.finishGame} disabled>Finish</Button>
         </div>
       </div>
     </div>);
