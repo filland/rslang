b8a0eb6785406c4a8d75b1d9fba8268c1a670a4d
GrootStudent <borodichalexander97@gmail.com>
2020-07-11 12:56:54 +0300

    fix: refactoring game

--- src/component/game/audioChallenge/components/Word/index.jsx
+++ src/component/game/audioChallenge/components/Word/index.jsx
@@ -5,21 +5,21 @@ import './styles.scss';
 const Word = ({
   wordTranslate,
   rightWord,
-  answer,
-  select,
+  // answer,
+  // select,
 }) => {
   const expression = () => {
-    let res = 'info';
-    if (answer === true) {
-      res = (rightWord) ? 'success' : 'light';
-    } else if (answer === false) {
-      if (select) {
-        res = 'danger';
-      } else {
-        res = (rightWord) ? 'info' : 'light';
-      }
-    }
-    return res;
+    //   let res = 'info';
+    //   if (answer === true) {
+    //     res = (rightWord) ? 'success' : 'light';
+    //   } else if (answer === false) {
+    //     if (select) {
+    //       res = 'danger';
+    //     } else {
+    //       res = (rightWord) ? 'info' : 'light';
+    //     }
+    //   }
+    //   return res;
   };
   return (
     <Button className="btn-word" variant={expression()}>
--- src/component/game/audioChallenge/components/Words/index.jsx
+++ src/component/game/audioChallenge/components/Words/index.jsx
@@ -6,31 +6,33 @@ import './styles.scss';
 const Words = ({
   words,
   rightWord,
-  setIsSelectAnswer,
-  isSelectAnswer,
+  // setIsSelectAnswer,
+  // isSelectAnswer,
 }) => {
-  const [isRightAnswer, setIsRightAnswer] = useState('');
-  const [isSelectWord, setIsSelectWord] = useState({});
+  // const [isRightAnswer, setIsRightAnswer] = useState('');
+  // const [isSelectWord, setIsSelectWord] = useState({});
 
   const templateWords = words.map(({ wordTranslate, id }) => (
     <Word
       key={id}
       rightWord={rightWord.id === id}
       wordTranslate={wordTranslate}
-      answer={(isSelectAnswer) ? isRightAnswer : ''}
-      select={isSelectWord.innerText === wordTranslate} />
+    // answer={(isSelectAnswer) ? isRightAnswer : ''}
+    // select={isSelectWord.innerText === wordTranslate}
+    />
   ));
 
-  const handleSelectAnswer = (e) => {
-    setIsSelectAnswer(true);
-    const selectWord = e.target;
-    setIsSelectWord(selectWord);
-    const answer = (rightWord.wordTranslate === selectWord.innerText);
-    return (answer) ? setIsRightAnswer(true) : setIsRightAnswer(false);
-  };
+  // const handleSelectAnswer = (e) => {
+  //   setIsSelectAnswer(true);
+  //   const selectWord = e.target;
+  //   setIsSelectWord(selectWord);
+  //   const answer = (rightWord.wordTranslate === selectWord.innerText);
+  //   return (answer) ? setIsRightAnswer(true) : setIsRightAnswer(false);
+  // };
 
   return (
-    <ButtonGroup aria-label="List words" onClick={handleSelectAnswer}>{templateWords}</ButtonGroup>
+    <ButtonGroup aria-label="List words">{templateWords}</ButtonGroup>
+    // <ButtonGroup aria-label="List words" onClick={handleSelectAnswer}>{templateWords}</ButtonGroup>
   );
 };
 
