f867d37105378a0dcc093079c6673683349bb03a
Student <borodichalexander97@gmail.com>
2020-07-15 15:19:25 +0300

    feat: add description game

--- src/component/game/audioChallenge/pages/menu/index.jsx
+++ src/component/game/audioChallenge/pages/menu/index.jsx
@@ -6,7 +6,21 @@ import './styles.scss';
 export default function Menu({ setCurrentPage }) {
   return (
     <div className="audioChallenge__menu">
-      <Button onClick={() => setCurrentPage(GAME_PAGE)}>Start</Button>
+      <div className="game-card">
+        <div className="description">
+          <h1>Аудиовызов</h1>
+          <h2>Цель - выбрать перевод слова по звучащему произношению.</h2>
+          <p>
+            <ul>
+              <li>В процессе игры звучит произношение слова на английском языке, нужно выбрать перевод слова из пяти предложенных вариантов ответа.</li>
+              <li>Слова можно угадывать, выбирая их как кликами мышкой, так и нажатием кнопок клавиатуры.</li>
+            </ul>
+          </p>
+        </div>
+        <div className="game-setup">
+          <Button onClick={() => setCurrentPage(GAME_PAGE)}>Start</Button>
+        </div>
+      </div>
     </div>
   );
 }
